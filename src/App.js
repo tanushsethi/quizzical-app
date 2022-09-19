074251faeefbf93d72e7e978e4d70191164b8f40
Tanush Sethi <tanushsethi55@gmail.com>
2022-09-19 17:30:53 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Overlay from "./components/Overlay"
+import Questions from "./components/Ouestions"
+
 
 function App() {
   return (
     <div className="app">
-      
+      <Overlay/>
+      <Questions/>
     </div>
   );
 }
