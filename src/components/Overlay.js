60ddf164df96c4c5a68c4cf531f14c25aaee0c7d
Tanush Sethi <tanushsethi55@gmail.com>
2022-09-21 17:56:17 +0530

    overlay component

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function Overlay() {
     return (
-        <></>
+        <div className="overlay">
+            <h1>Quizzical</h1>
+            <p>Quiz on the go!</p>
+        </div>
     )
 }
 
